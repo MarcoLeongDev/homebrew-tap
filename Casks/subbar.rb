eed1c178f85f22886d60ee5ed8ad32e9760ee9e8
Marco Leong <4134457+MarcoLeongDev@users.noreply.github.com>
2026-08-31 16:00:27 +1000

    cask: SubBar 1.0.35 (re-center panel on tray title update)

--- Casks/subbar.rb
+++ Casks/subbar.rb
@@ -1,6 +1,6 @@
 cask "subbar" do
-  version "1.0.32"
-  sha256 "8be513f1dc7b7b87107eb70787c1d11c18e8d231afcb3f5d7e0be47e3d2875b6"
+  version "1.0.35"
+  sha256 "d4d27ad42a5cb74db2f435d06c059ac4d5455b0cce2e49c35eb6c18dd9d21ba6"
 
   url "https://cdn.jsdelivr.net/gh/MarcoLeongDev/homebrew-tap@v#{version}/dist/SubBar_#{version}_universal.dmg"
   name "SubBar"
